a9cc6227d4c4ba5705522012b4d85c0887a316d1
Caio Caminha <caio.caminha@globalpay.com>
2024-09-25 09:51:27 -0300

    feat: adding initial

---
+++ src/main/java/com/example/releasenotesexample/ReleaseNotesExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.releasenotesexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseNotesExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReleaseNotesExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/releasenotesexample/ReleaseNotesExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.releasenotesexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseNotesExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
